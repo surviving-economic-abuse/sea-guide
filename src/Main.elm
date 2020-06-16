a9017f4d20377a89933941e1dc59a7c6f51331dc
Nikomus <nick.wade@live.co.uk>
2020-06-16 09:46:36 +0100

    Test component styling with elm-css

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Css exposing (..)
-import Html
+import Css.Global exposing (everything, global, typeSelector)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (css, href, src)
 import Theme exposing (colours)
@@ -47,7 +47,10 @@ viewDocument model =
 
 view : Model -> Html Msg
 view model =
-    layout [] []
+    layout []
+        [ globalStyles
+        , whatisea
+        ]
 
 
 layout : List (Attribute msg) -> List (Html msg) -> Html msg
@@ -56,8 +59,56 @@ layout =
         [ margin2 zero auto
         , maxWidth (px 800)
         , width (pct 100)
-        , backgroundColor (hex "cCc")
+        , backgroundColor (hex "fff")
         , displayFlex
         , flexDirection column
         , minHeight (vh 100)
+        ]
+
+
+whatisea : Html msg
+whatisea =
+    div []
+        [ header []
+            [ h1 [ css [ pageHeadingStyles ] ] [ text "What is Economic Abuse?" ]
+            , p [ css [ pageDescriptionStyles ] ] [ text "A concise definition of economic abuse in accessible language. ", a [ href "/somewhere" ] [ text "somewhere" ] ]
+            ]
+        , ul []
+            [ li [] [ text "A Category" ]
+            , li [] [ text "A Category" ]
+            , li [] [ text "A Category" ]
+            ]
+        ]
+
+
+eacategories : Html msg
+eacategories =
+    div [] []
+
+
+pageHeadingStyles : Style
+pageHeadingStyles =
+    Css.batch
+        [ color Theme.colours.purple
+        , fontFamilies [ "Raleway-ExtraBold", "sansSerif" ]
+        , fontSize (rem 2.5)
+        , margin (rem 2)
+        , textAlign center
+        ]
+
+
+pageDescriptionStyles : Style
+pageDescriptionStyles =
+    Css.batch
+        [ color Theme.colours.purple
+        , fontFamilies [ "Raleway-Bold", "sansSerif" ]
+        , fontSize (rem 1.8)
+        ]
+
+
+globalStyles : Html msg
+globalStyles =
+    global
+        [ typeSelector "body" [ backgroundColor (hex "eee") ]
+        , everything [ color Theme.colours.grey, fontFamilies [ "sansSerif" ] ]
         ]
