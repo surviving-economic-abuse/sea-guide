98eacee958890985b77b3a0dbcd4340376727b75
katjam <katja@neontribe.co.uk>
2020-06-17 16:40:57 +0100

    Add draft Keys for Not Alone & Definition

---
+++ src/Copy/Keys.elm
@@ -0,0 +1,79 @@
+module Copy.Keys exposing (Key(..))
+
+
+type Key
+    = SiteTitle
+      -- Not Alone page
+    | NotAloneTitle
+    | QuoteRelatable1
+    | QuoteHopeful1
+    | QuoteStatement1
+    | QuoteName1
+    | QuoteAge1
+    | QuoteRelatable2
+    | QuoteHopeful2
+    | QuoteStatement2
+    | QuoteName2
+    | QuoteAge2
+    | QuoteRelatable3
+    | QuoteHopeful3
+    | QuoteStatement3
+    | QuoteName3
+    | QuoteAge3
+    | QuoteRelatable4
+    | QuoteHopeful4
+    | QuoteStatement4
+    | QuoteName4
+    | QuoteAge4
+    | QuoteRelatable5
+    | QuoteHopeful5
+    | QuoteStatement5
+    | QuoteName5
+    | QuoteAge5
+    | QuoteRelatable6
+    | QuoteHopeful6
+    | QuoteStatement6
+    | QuoteName6
+    | QuoteAge6
+    | ToDefinitionFromNotAloneLink
+    | EmergencyButton
+    | EmergencyReassure
+    | EmergencyContact999
+    | EmergencyContactDV
+      -- Definition page
+    | DefinitionTitle
+    | DefintionConcise
+    | DefinitionMoreLink
+    | DefinitionCategoryTitle1
+    | DefinitionCategoryInfo1
+    | DefinitionCategoryQuoteOne1
+    | DefinitionCategoryQuoteTwo1
+    | DefinitionCategoryQuoteThree1
+    | DefinitionCategoryTitle2
+    | DefinitionCategoryInfo2
+    | DefinitionCategoryQuoteOne2
+    | DefinitionCategoryQuoteTwo2
+    | DefinitionCategoryQuoteThree2
+    | DefinitionCategoryTitle3
+    | DefinitionCategoryInfo3
+    | DefinitionCategoryQuoteOne3
+    | DefinitionCategoryQuoteTwo3
+    | DefinitionCategoryQuoteThree3
+    | DefinitionCategoryTitle4
+    | DefinitionCategoryInfo4
+    | DefinitionCategoryQuoteOne4
+    | DefinitionCategoryQuoteTwo4
+    | DefinitionCategoryQuoteThree4
+    | DefinitionCategoryTitle5
+    | DefinitionCategoryInfo5
+    | DefinitionCategoryQuoteOne5
+    | DefinitionCategoryQuoteTwo5
+    | DefinitionCategoryQuoteThree5
+    | SplitterAffirmation
+    | ToGetHelpFromDefinitionLink
+    | ToHelpSelfFromDefinitionLink
+
+
+
+-- Get Help page
+-- Help Self page
---
+++ src/Copy/Text.elm
@@ -0,0 +1,14 @@
+module Copy.Text exposing (t)
+
+import Copy.Keys exposing (Key(..))
+
+
+
+-- The translate function
+
+
+t : Key -> String
+t key =
+    case key of
+        SiteTitle ->
+            "[cCc] SEA MAP"
