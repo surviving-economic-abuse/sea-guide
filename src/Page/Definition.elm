36ab163e9517b091a46d8fdf75f8689a4f50758e
Nikomus <nick.wade@live.co.uk>
2020-06-30 10:24:55 +0100

    Add navigation styling classes

--- src/Page/Definition.elm
+++ src/Page/Definition.elm
@@ -3,7 +3,7 @@ module Page.Definition exposing (Model, Msg, init, update, view)
 import Copy.Keys exposing (Key(..))
 import Copy.Text exposing (t)
 import Css exposing (..)
-import Html.Styled exposing (Html, a, div, h1, h2, header, li, p, text, ul)
+import Html.Styled exposing (Html, a, div, h1, h2, header, li, nav, p, text, ul)
 import Html.Styled.Attributes exposing (css, href)
 import Theme exposing (colours, pageHeadingStyle)
 
@@ -48,10 +48,20 @@ view model =
             , li [ css [ categoryStyle ] ] [ h2 [] [ text (t DefinitionCategory4Title) ] ]
             , li [ css [ categoryStyle ] ] [ h2 [] [ text (t DefinitionCategory5Title) ] ]
             ]
-        , p [] [ text (t SplitterAffirmation) ]
-        , ul []
-            [ li [] [ a [ href (t HelpSelfGridPageSlug) ] [ text (t ToHelpSelfFromDefinitionLink) ] ]
-            , li [] [ a [ href (t GetHelpPageSlug) ] [ text (t ToGetHelpFromDefinitionLink) ] ]
+        , p []
+            [ text (t SplitterAffirmation) ]
+        , nav
+            [ css [ navStyle ] ]
+            [ ul [ css [ navListStyle ] ]
+                [ li [ css [ navItemStyle ] ]
+                    [ a [ href (t HelpSelfGridPageSlug), css [ linkStyle ] ] [ text (t ToHelpSelfFromDefinitionLink) ]
+                    , p [] [ text "[cCc] Additional text here." ]
+                    ]
+                , li [ css [ navItemStyle ] ]
+                    [ a [ href (t GetHelpPageSlug), css [ linkStyle ] ] [ text (t ToGetHelpFromDefinitionLink) ]
+                    , p [] [ text "[cCc] Additional text here." ]
+                    ]
+                ]
             ]
         ]
 
@@ -68,5 +78,42 @@ introStyle =
         [ color colours.purple
         , fontFamilies [ "Raleway", sansSerif.value ]
         , fontSize (rem 1.25)
+        , margin2 (rem 2) zero
+        ]
+
+
+
+-- End of page navigation styling
+
+
+navStyle : Style
+navStyle =
+    Css.batch
+        [ margin2 (rem 2) zero
+        ]
+
+
+navListStyle : Style
+navListStyle =
+    Css.batch
+        [ listStyle none
+        , displayFlex
+        , flexWrap wrap
+        ]
+
+
+navItemStyle : Style
+navItemStyle =
+    Css.batch
+        [ Css.borderRadius (rem 1)
+        , flex3 (int 1) zero (pct 100)
+        , margin2 (rem 1) zero
+        ]
+
+
+linkStyle : Style
+linkStyle =
+    Css.batch
+        [ fontWeight bold
         , margin2 (rem 2) zero
         ]
