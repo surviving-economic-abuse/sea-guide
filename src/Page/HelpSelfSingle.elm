2ddf264e33ca69d20143ec6c633abb3b9c759998
katjam <katja@neontribe.co.uk>
2020-07-06 11:08:39 +0100

    Only import the Key types to Help self single

--- src/Page/HelpSelfSingle.elm
+++ src/Page/HelpSelfSingle.elm
@@ -1,6 +1,6 @@
 module Page.HelpSelfSingle exposing (Model, Msg(..), update, view)
 
-import Copy.Keys exposing (..)
+import Copy.Keys exposing (Key(..))
 import Copy.Text exposing (t)
 import Css exposing (..)
 import Css.Media as Media exposing (minWidth, only, screen, withMedia)
