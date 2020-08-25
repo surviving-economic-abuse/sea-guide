feaac9dcada856f6eb5e7a931d2b724d9ae642b8
Nikomus <nick.wade@live.co.uk>
2020-08-25 16:17:47 +0100

    add navlink into route

--- src/Route.elm
+++ src/Route.elm
@@ -1,12 +1,12 @@
-module Route exposing (Route(..), fromUrl, toString)
+module Route exposing (Direction(..), Route(..), fromUrl, renderNavLink, toString)
 
 import Copy.Keys exposing (Key(..))
 import Copy.Text exposing (t)
-import Html.Styled exposing (..)
-import Page.Definition as Definition
-import Page.HelpSelfSingle as HelpSelfSingle
-import Page.NotAlone as NotAlone
-import Set
+import Css exposing (..)
+import Css.Transitions exposing (transition)
+import Html.Styled exposing (Html, a, img, span, text)
+import Html.Styled.Attributes exposing (alt, css, href, src)
+import Theme exposing (grey, lightPurple, purple, white)
 import Url
 import Url.Parser as Parser exposing ((</>), Parser, map, oneOf, s, string, top)
 
@@ -42,6 +42,80 @@ toString route =
 
         NotAlone ->
             t NotAlonePageSlug
+
+
+
+-- links and buttons
+
+
+type Direction
+    = Back
+    | Forward
+    | None
+
+
+renderNavLink : Direction -> Key -> Key -> Html msg
+renderNavLink direction url copyKey =
+    case direction of
+        Back ->
+            a [ href (t url), css [ navLinkStyle ] ]
+                [ img [ src "Arrow.svg", alt "", css [ backArrowStyle ] ] []
+                , span [] [ text (t copyKey) ]
+                ]
+
+        Forward ->
+            a [ href (t url), css [ navLinkStyle ] ]
+                [ span [] [ text (t copyKey) ]
+                , img [ src "Arrow.svg", alt "", css [ forwardArrowStyle ] ] []
+                ]
+
+        None ->
+            a [ href (t url), css [ navLinkStyle ] ]
+                [ span [] [ text (t copyKey) ]
+                ]
+
+
+navLinkStyle : Style
+navLinkStyle =
+    batch
+        [ alignItems center
+        , backgroundColor purple
+        , borderRadius (rem 100)
+        , color white
+        , displayFlex
+        , fontSize (rem 1)
+        , fontWeight (int 700)
+        , justifyContent center
+        , minHeight (rem 3.375)
+        , padding2 (rem 0.5) (rem 1)
+        , textAlign center
+        , textDecoration none
+        , hover
+            [ backgroundColor lightPurple
+            , color grey
+            ]
+        , transition
+            [ Css.Transitions.backgroundColor 200
+            , Css.Transitions.color 200
+            ]
+        ]
+
+
+forwardArrowStyle : Style
+forwardArrowStyle =
+    batch
+        [ height (rem 1.8)
+        , marginLeft (rem 1.2)
+        ]
+
+
+backArrowStyle : Style
+backArrowStyle =
+    batch
+        [ height (rem 1.8)
+        , marginRight (rem 1.2)
+        , transform (rotate (deg 180))
+        ]
 
 
 
