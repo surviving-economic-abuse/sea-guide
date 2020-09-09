adfd70ee36f83c081ac745ff6b108ea9c49399fe
katjam <katja@neontribe.co.uk>
2020-09-09 14:30:55 +0100

    Remove sea-map pathed images and routes

--- src/Route.elm
+++ src/Route.elm
@@ -138,10 +138,4 @@ routeParser =
         , Parser.map GetHelp (Parser.s (t GetHelpPageSlug))
         , Parser.map HelpSelfGrid (Parser.s (t HelpSelfGridPageSlug))
         , Parser.map HelpSelfSingle (Parser.s "help-self" </> string)
-
-        -- Hardcoded to include staging prefix
-        , Parser.map Definition (Parser.s "sea-map" </> Parser.s (t DefinitionPageSlug))
-        , Parser.map GetHelp (Parser.s "sea-map" </> Parser.s (t GetHelpPageSlug))
-        , Parser.map HelpSelfGrid (Parser.s "sea-map" </> Parser.s (t HelpSelfGridPageSlug))
-        , Parser.map HelpSelfSingle (Parser.s "sea-map" </> Parser.s "help-self" </> string)
         ]
