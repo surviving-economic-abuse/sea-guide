db5674a13fe0187d62e07eae23180afb61816898
katjam <katja@neontribe.co.uk>
2020-09-22 13:11:41 +0100

    Desktop/ Tablet view for 2 column definition page with pops

--- src/View/Definition.elm
+++ src/View/Definition.elm
@@ -3,7 +3,7 @@ module View.Definition exposing (view)
 import Copy.Keys exposing (Key(..))
 import Copy.Text exposing (t)
 import Css exposing (..)
-import Html.Styled exposing (Html, b, blockquote, button, dd, div, dl, dt, h1, h2, header, img, li, nav, p, text, ul)
+import Html.Styled exposing (Html, b, blockquote, button, dd, div, dl, dt, h1, h2, header, img, li, nav, p, span, text, ul)
 import Html.Styled.Attributes exposing (alt, css, id, src, tabindex)
 import Html.Styled.Attributes.Aria exposing (ariaControls, ariaExpanded)
 import Html.Styled.Events exposing (onClick)
@@ -25,11 +25,9 @@ view hasConsented model =
                         , img [ css [ imageStyle ], src "/Definition.svg" ] []
                         ]
                     , div [ css [ introParagraphStyle ] ]
-                        [ div [ css [ popStyle ] ]
-                            [ p [] (renderWithKeywords (t DefinitionConciseP2))
-                            , p [] (renderWithKeywords (t DefinitionConciseP3))
-                            ]
-                        , p [] (renderWithKeywords (t DefinitionConciseP4))
+                        [ div [ css [ popRightStyle ] ] [ p [] (renderWithKeywords (t DefinitionConciseP2)) ]
+                        , div [ css [ popLeftStyle ] ] [ p [ css [ marginTop (rem 1) ] ] (renderWithKeywords (t DefinitionConciseP3)) ]
+                        , p [ css [ marginTop (rem 2.5) ] ] (renderWithKeywords (t DefinitionConciseP4))
                         ]
                     ]
                 ]
@@ -128,7 +126,7 @@ introStyle =
         [ displayFlex
         , flexWrap wrap
         , alignItems start
-        , margin2 (rem 2) (rem 1)
+        , margin2 zero (rem 1)
         ]
 
 
@@ -147,12 +145,31 @@ imageStyle : Style
 imageStyle =
     batch
         [ margin auto
-        , maxWidth (pct 60)
+        , maxWidth (pct 80)
+        , paddingTop (rem 1)
         , width (rem 20)
         ]
 
 
-popStyle : Style
-popStyle =
+popRightStyle : Style
+popRightStyle =
     batch
-        [ backgroundImage (url "/Pop.svg"), backgroundRepeat noRepeat ]
+        [ backgroundImage (url "/Pop_Right.svg")
+        , backgroundRepeat noRepeat
+        , backgroundPosition2 (pct 100) zero
+        , backgroundSize (px 60)
+        , margin4 (px -30) (px -30) zero zero
+        , padding4 (px 30) (px 30) zero zero
+        ]
+
+
+popLeftStyle : Style
+popLeftStyle =
+    batch
+        [ backgroundImage (url "/Pop_Left.svg")
+        , backgroundRepeat noRepeat
+        , backgroundPosition2 zero (pct 100)
+        , backgroundSize (px 40)
+        , margin4 zero zero (px -30) (px -30)
+        , padding4 zero zero (px 30) (px 30)
+        ]
