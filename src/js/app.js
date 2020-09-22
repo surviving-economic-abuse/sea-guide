2eae32f9b8eb8b8fef6aff133a6f59a5b9e58034
katjam <katja@neontribe.co.uk>
2020-09-22 14:50:30 +0100

    Wrap gtag calls in window not undefined

--- src/js/app.js
+++ src/js/app.js
@@ -19,9 +19,13 @@ elm.ports.saveConsent.subscribe(function (hasConsented) {
 
 // Google analytics subscribe to page changes and custom events
 elm.ports.updateAnalyticsPage.subscribe(function (page) {
-  gtag("config", "UA-30970110-8", { "page_path" : page });
+    if (typeof window !="undefined") {
+      gtag("config", "UA-30970110-8", { "page_path" : page });
+    }
 });
 
 elm.ports.updateAnalyticsEvent.subscribe(function (gaEvent) {
-  gtag("event", gaEvent.action, { "event_category" : gaEvent.category, "event_label" : gaEvent.label });
+  if (typeof window != "undefined") {
+    gtag("event", gaEvent.action, { "event_category" : gaEvent.category, "event_label" : gaEvent.label });
+  }
 });
